9c598261c0e8573dc5af2b62b18842fcafe08f0a
Evan Carmi <evan@ecarmi.org>
2013-05-18 13:31:35 -0400

    Add locahost to db settings

--- wespeople/wespeople/settings.py
+++ wespeople/wespeople/settings.py
@@ -16,7 +16,7 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'wespeople_user',
         'PASSWORD': 'wespeople_user',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
