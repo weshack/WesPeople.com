423c5edc14980f33191d7f95f1116e17f184ef23
Lisa Sy <lsy@Lisas-MacBook-Air.local>
2013-05-18 12:57:43 -0400

    in VIEWS folder, I've set up a login view in index.html. It's very barebones but it will do for now

---
+++ views/js/script.js
@@ -0,0 +1,13 @@
+jQuery.noConflict();
+jQuery(document).ready(function($){
+  $("#login-toggle").click(function() {
+    $('.login-container').slideToggle(300,function(){
+      $("#login-toggle").text('LOG IN').stop();
+    });
+    $('.signup-container').slideToggle(300, function(){
+       $("#login-toggle").text('sign up now').stop();
+    });
+  });
+});
+
+
