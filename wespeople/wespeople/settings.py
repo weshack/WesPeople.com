41a318f33a0027eeae28266557e578cfdedd6d27
Evan Carmi <evan@ecarmi.org>
2013-05-18 13:39:45 -0400

    Changed engine to postgis

--- wespeople/wespeople/settings.py
+++ wespeople/wespeople/settings.py
@@ -11,7 +11,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'wespeople',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'wespeople_user',
