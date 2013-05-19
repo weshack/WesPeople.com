0b31811ec0d92c8aa9ee50c8e90d7f35337bc48f
Lisa Sy <lsy@Lisas-MacBook-Air.local>
2013-05-19 17:48:15 -0400

    working on RWD with frustration

--- wespeople/wespeople/static/js/script.js
+++ wespeople/wespeople/static/js/script.js
@@ -29,7 +29,7 @@ function load_people(url, filters) {
         var country = val.country;
         var marker = new L.marker([lat, lng]);
         // var marker = L.circleMarker([lat, lng], 200)
-        marker.bindPopup("<p><b>" + name + "</b>" + " " + year + "</br />" + degree1 + "<br /> " + degree2 + "<br /> " + degree3 + "<br />" + industry + "<br />" + city + ", " + state + ", " + country + "</p>");
+        marker.bindPopup("<p><b>" + name + "</b>" + "<p>Class of " + year + "<br />Major: " + degree1 + "<br />" + degree2 + "<br /> " + degree3 + "<br />" + industry + "<br />" + city + ", " + state + ", " + country + "</p>");
         markers.addLayer(marker);
         people.push(val)
         jQuery("span#num_people").html(people.length);
