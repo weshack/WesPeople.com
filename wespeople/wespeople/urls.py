4cb2ab28e89e086a4a6b1fc61ea886c0ce629121
Brian <bgapinski@wesleyan.edu>
2013-05-18 23:08:21 -0400

    plugged in the login page

--- wespeople/wespeople/urls.py
+++ wespeople/wespeople/urls.py
@@ -5,6 +5,7 @@ from tastypie.api import Api
 from maps.api import PersonResource
 from maps.models import Person
 from django.conf import settings
+from django.contrib.auth.views import login, logout
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -27,4 +28,7 @@ urlpatterns = patterns('',
     url(r'^api/', include(people.urls)),
     (r'^static/(?P<path>.*)$', 'django.views.static.serve',
                 {'document_root': settings.MEDIA_ROOT}),
+    (r'^accounts/login/$', login, 
+            {'template_name':'login.html'}),
+    (r'^accounts/logout/$', logout),
 )
