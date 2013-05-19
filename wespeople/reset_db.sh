75aaed998fb8af4c565288c22b4300a996c62a11
Evan Carmi <evan@ecarmi.org>
2013-05-19 13:47:58 -0400

    updated reset

--- wespeople/reset_db.sh
+++ wespeople/reset_db.sh
@@ -1,4 +1,4 @@
-./manage.py sqlclear map | ./manage.py dbshell
+./manage.py sqlclear maps | ./manage.py dbshell
 ./manage.py syncdb
 pip install -r ../REQUIREMENTS.txt
 #./manage.py loaddata fixtures/db.json
