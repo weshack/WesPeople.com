24b79d96019587bf95920d3eb18d152e4b178ce1
Evan Carmi <evan@ecarmi.org>
2013-05-19 05:41:32 -0400

    updated code

--- wespeople/wespeople/static/js/script.js
+++ wespeople/wespeople/static/js/script.js
@@ -1,4 +1,6 @@
 var markers;
+var people = [];
+
 jQuery.noConflict();
 jQuery(document).ready(function($){
   $("#login-toggle").click(function() {
@@ -11,22 +13,43 @@ jQuery(document).ready(function($){
   });
 
   markers = new L.MarkerClusterGroup();
-  load_people("/api/geoperson?preferred_class_year__gte=2000", "");
+  load_people("/api/geoperson?preferred_class_year__gte=2010", "");
+  jQuery('#location-search').submit(function(e) {
+      var s = $('#myForm :input');
+      console.log(s);
+      e.preventDefault();
+      $.getJSON(("http://open.mapquestapi.com/geocoding/v1/address?key=Fmjtd%7Cluub2dutn9%2Cbn%3Do5-9u25gr&location=" + s + "&callback=renderGeocode"), function(data) {
+        console.log(data);
+          var values = {};
+              s.each(function() {
+                        values[this.name] = $(this).val();
+                            });
 
+      });
+      return false;
+  });
 });
 
 function load_people(filters, url) {
-  var people = jQuery.getJSON((url + filters), function(data) {
+  jQuery.getJSON((url + filters), function(data) {
     console.log( "success" );
       jQuery.each(data.objects, function (key, val) {
-        lng = val.location.coordinates[0];
-        lat = val.location.coordinates[1];
-        name = val.first_name + " " + val.last_name;
-        year = val.preferred_class_year
+        var lng = val.location.coordinates[0];
+        var lat = val.location.coordinates[1];
+        var name = val.first_name + " " + val.last_name;
+        var year = val.preferred_class_year;
+        var degree1 = val.wesleyan_degree_1_major_1;
+        var degree2 = val.wesleyan_degree_1_major_2;
+        var degree3 = val.wesleyan_degree_1_major_3;
+        var industry = val.industry;
+        var city = val.city;
+        var state = val.state;
+        var country = val.country;
         var marker = new L.marker([lat, lng]);
         // var marker = L.circleMarker([lat, lng], 200)
-        marker.bindPopup("<p><b>" + name + "</b>" + " " + year + "</br />" + "</p>");
+        marker.bindPopup("<p><b>" + name + "</b>" + " " + year + "</br />" + degree1 + "<br /> " + degree2 + "<br /> " + degree3 + "<br />" + industry + "<br />" + city + ", " + state + ", " + country + "</p>");
         markers.addLayer(marker);
+        people.push(val)
       });
     map.addLayer(markers); 
   })
