832958527ea896909c1af4b65b34552c1c14f3a7
Brian <bgapinski@wesleyan.edu>
2013-05-18 23:50:24 -0400

    relinked the javascript

--- wespeople/maps/views.py
+++ wespeople/maps/views.py
@@ -1,11 +1,15 @@
 # Create your views here.
-from django.shortcuts import render_to_response, redirect
-from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render_to_response, redirect, render
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
 from django.db import models
+from django.contrib.auth.decorators import login_required
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
 
 from maps.models import *
 
 
+@login_required
 def map_test(request):
     """
     display something
@@ -15,3 +19,13 @@ def map_test(request):
     template_values = {'test': 'test',}
 
     return render_to_response('map.html', template_values)
+
+def register(request):
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      new_user = form.save()
+      return HttpResponseRedirect("/books/")
+  else:
+    form = UserCreationForm()
+  return render(request, "/accounts/login.html", {'form': form,})
