f3c92228b184d8e6db15d5e03a71c99879b19072
Evan Carmi <evan@ecarmi.org>
2013-05-19 03:05:18 -0400

    Changed media settings

--- wespeople/wespeople/urls.py
+++ wespeople/wespeople/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.gis import admin
 from django.conf.urls.defaults import *
-from django.conf.urls.static import static
 from tastypie.api import Api
 from maps.api import PersonResource
-from maps.views import register
 from maps.models import Person
 from django.conf import settings
 from django.contrib.auth.views import login, logout
@@ -27,17 +25,11 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     (r'^filter/$', 'maps.views.search'),
     url(r'^api/', include(people.urls)),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+                {'document_root': settings.MEDIA_ROOT}),
     (r'^accounts/login/$', login,
             {'template_name':'login.html'}),
-    (r'^accounts/login/$', login),
     (r'^accounts/logout/$', logout,
-          {'next_page': '/'}),
-    (r'^accounts/register/$', register),
+            {'next_page': '/accounts/login'}),
     (r'^$', 'maps.views.index'),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^static-admin/media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.ADMIN_MEDIA_ROOT, 'show_indexes': True}),
-    )
+)
